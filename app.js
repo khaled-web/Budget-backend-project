5114fdc046b660a278007e20d3cc5dea00a6aac0
khaled-web <eng.khaled_ok@outlook.com>
2020-08-10 20:05:10 +0200

    update the percentage of each item

--- app.js
+++ app.js
@@ -319,7 +319,7 @@ var controller = (function(budgetCtrl, uictrl) {
     var percentage = budgetCtrl.getPercentage();
 
     // update the percentage in UI.
-    console.log(percentage);
+    uictrl.displayPercentage(percentage);
 
   }
 
